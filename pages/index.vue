c289b3be68af3e0f5fd9905d1ea0b2da1932ca79
Teodor Grigor <open-source@teogor.dev>
2024-02-17 20:44:03 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,2 +1,17 @@
 <template>
+  <div>
+    Welcome Home, Teodor!
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      pageTitle: 'My Index Page Title',
+      keywords: 'nuxt, seo, meta tags',
+      description: 'This is my homepage description.',
+    };
+  },
+};
+</script>
