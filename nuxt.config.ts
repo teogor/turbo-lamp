3e91ad00e990a56477b579447034a89966620fc7
Teodor Grigor <open-source@teogor.dev>
2024-02-17 17:51:00 +0200

    s

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,12 @@
+const icons = [
+  { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+  { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+  { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+  { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+  { rel: 'manifest', href: '/site.webmanifest' },
+];
+
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
@@ -11,9 +20,14 @@ export default defineNuxtConfig({
         { hid: 'description', name: 'description', content: 'Teogor\' personal website!' },
         { name: 'format-detection', content: 'telephone=no' }
       ],
-      link: [
+      link: icons.map(icon => ({
+        rel: icon.rel,
+        type: icon.type,
+        sizes: icon.sizes ? icon.sizes : undefined,
+        href: icon.href,
+      })) + [
         { rel: 'icon', type: 'image/svg', href: '/favicon.ico' }
-      ]
+      ],
     },
   },
   modules: [
