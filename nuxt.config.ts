d38ba3925ee9eaa773720602c01877ecf73ae2ff
Teodor Grigor <open-source@teogor.dev>
2024-02-17 20:40:33 +0200

    s

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,7 +28,9 @@ export default defineNuxtConfig({
           sizes: icon.sizes ? icon.sizes : undefined,
           href: baseUrlDef + icon.href,
         })),
-        { rel: 'stylesheet', href: 'https://your-stylesheet.css' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&family=Sanchez:ital@0;1&display=swap' },
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
       ],
     },
     baseURL: baseUrlDef
