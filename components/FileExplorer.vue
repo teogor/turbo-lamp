1d039c1e9d6ac7ded6b3066588ee86608f63a841
Teodor Grigor <open-source@teogor.dev>
2024-02-17 18:30:59 +0200

    Update FileExplorer.vue

--- components/FileExplorer.vue
+++ components/FileExplorer.vue
@@ -1,51 +1,41 @@
 <template>
-  <div>
-    <h2>File Explorer</h2>
-    <ul v-if="entries.length" class="file-list">
-      <li v-for="(entry, index) in entries" :key="index">
-        <a v-if="entry.type === 'dir'" :href="`javascript:void(0)`" @click="changeDirectory(entry.name)">{{ entry.name }}</a>
-        <span v-else>{{ entry.name }}</span>
-      </li>
-    </ul>
-    <p v-else>No entries found.</p>
+  <div class="container">
+    <div v-for="(filteredArticles, categoryKey) in groupedCategories" :key="categoryKey">
+      <list-item v-for="article in filteredArticles" :key="article.slug">
+        <template #title>
+          {{ article.title }}
+        </template>
+        <template #content>
+          <div> {{ article.description }}</div>
+        </template>
+        <br>
+      </list-item>
+      <hr>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      entries: [],
-      currentPath: '/'
-    };
-  },
-  methods: {
-    async fetchDirectoryContent() {
-      // Implement your logic to fetch directory content based on your environment
-      // Example using Node.js fs module:
-      const dir = await fs.promises.readdir(this.currentPath);
-      this.entries = dir.map(async name => ({
-        name,
-        type: await fs.promises.stat(path.join(this.currentPath, name)).then(stat => stat.isDirectory() ? 'dir' : 'file'),
-      }));
-    },
-    changeDirectory(name) {
-      this.currentPath = path.join(this.currentPath, name);
-      this.fetchDirectoryContent();
-    }
-  },
-  mounted() {
-    this.fetchDirectoryContent();
-  }
-};
-</script>
+  async asyncData ({ $content }) {
+    const articles = await $content('', { deep: true })
+        .only(['title', 'description', 'img', 'slug', 'cat', 'dir'])
+        .sortBy('createdAt', 'asc')
+        .fetch()
 
-<style scoped>
-.file-list {
-  list-style: none;
-  padding: 0;
+    return { articles }
+  },
+  computed: {
+    groupedCategories () {
+      console.log("articles")
+      console.log(articles)
+      return this.articles.reduce((finalObject, obj) => {
+        const directory = obj.dir
+        finalObject[directory] ?? (finalObject[directory] = [])
+        finalObject[directory].push(obj)
+        return finalObject
+      }, {})
+    }
+  }
 }
-.file-list li {
-  margin-bottom: 5px;
-}
-</style>
+</script>
