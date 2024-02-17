614c8d5fcfa9393f9464d2abccc795bb8f204b84
Teodor Grigor <open-source@teogor.dev>
2024-02-17 17:45:12 +0200

    s

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,6 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/svg', href: '/favicon.svg' }
       ]
     },
-    // baseURL: 'https://teogor.dev/turbo-lamp/'
   },
   modules: [
   ],
